421d941d7b2223642b662ea007e934cf87461eff
GaniFatur <101238372+GaniFatur@users.noreply.github.com>
2022-03-09 12:41:26 +0700

    Add files via upload

---
+++ src/main/java/id/ac/uin/gani/demo/Demo1Application.java
@@ -0,0 +1,13 @@
+package id.ac.uin.gani.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo1Application.class, args);
+	}
+
+}
